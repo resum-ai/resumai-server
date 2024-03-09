748d29f4218980372021b347f9c874e03743a328
yjoonjang <yjoonjang34@gmail.com>
2024-03-10 00:52:34 +0900

    deploy: allowed hosts 전체로 설정

--- resumai/settings/base.py
+++ resumai/settings/base.py
@@ -20,7 +20,7 @@ SECRET_KEY = f"{os.environ.get('DJANGO_SECURE_KEY')}"
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
