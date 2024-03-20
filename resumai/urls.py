e593e9a718546ef47f5fce04a73f6e81bad37615
yjoonjang <yjoonjang34@gmail.com>
2024-03-21 02:37:09 +0900

    feat: home 화면 추가

--- resumai/urls.py
+++ resumai/urls.py
@@ -3,6 +3,9 @@ from django.urls import path
 from django.conf import settings
 from django.conf.urls.static import static
 
+from .views import home
+
 urlpatterns = [
+    path("", home, name="home"),
     path("admin/", admin.site.urls),
 ]
