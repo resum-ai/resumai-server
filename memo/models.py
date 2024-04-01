18271e3b946bdd9be55767fb3cffa16ec3646201
yjoonjang <yjoonjang34@gmail.com>
2024-04-01 21:02:54 +0900

    feat: memo 모델 정의

--- memo/models.py
+++ memo/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.conf import settings
+from django.utils import timezone
 
-# Create your models here.
+
+class Memo(models.Model):
+    user = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE)
+    created_at = models.DateTimeField(auto_now_add=True)
+    title = models.CharField(max_length=255)
+    content = models.TextField()
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title
