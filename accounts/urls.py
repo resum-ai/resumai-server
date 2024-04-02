6c686ed13141aa3fcde829f71247e84576bd2a48
yjoonjang <yjoonjang34@gmail.com>
2024-04-02 20:07:07 +0900

    fix: client에서 code 요청 보내도록 수정

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,8 @@ from accounts import views
 
 # TODO swagger에 뜨는 api 관리
 urlpatterns = [
-    path("kakao/login/", views.kakao_login, name="kakao_login"),
-    path("kakao/callback/", views.kakao_callback, name="kakao_callback"),
+    path("kakao/", views.kakao_login, name="kakao_login"),
+    path("kakao/login/", views.kakao_callback, name="kakao_callback"),
     path(
         "kakao/login/finish/",
         views.KakaoLoginView.as_view(),
--- accounts/views.py
+++ accounts/views.py
@@ -47,20 +47,23 @@ KAKAO_CALLBACK_URI = BASE_URL + "accounts/kakao/callback/"
 REST_API_KEY = env("KAKAO_REST_API_KEY")
 CLIENT_SECRET = env("KAKAO_CLIENT_SECRET_KEY")
 
-
-# @extend_schema(
-#     summary="카카오 로그인",
-#     description="카카오 로그인 페이지로 리다이렉트하여, 정보를 입력하면 카카오 **access_token, code**를 반환합니다.",
-#     responses={200: KakaoTokenSerializer},
-# )
 @extend_schema(exclude=True)
 def kakao_login(request):
     return redirect(
         f"https://kauth.kakao.com/oauth/authorize?client_id={REST_API_KEY}&redirect_uri={KAKAO_CALLBACK_URI}&response_type=code"
     )
 
-
+@extend_schema(
+    summary="카카오 로그인",
+    description="code를 GET 요청으로 보내면 access token, code를 반환합니다.",
+    parameters=[
+        OpenApiParameter(
+            name="code", type=str, description="발급받은 카카오의 code 입니다."
+        ),
+    ],
+)
 @permission_classes([AllowAny])
+@api_view(["GET"])
 def kakao_callback(request):
     code = request.GET.get("code")
 
@@ -93,54 +96,6 @@ def kakao_callback(request):
     data = {"access_token": access_token, "code": code}
     # TODO 유저 프로필 이미지 저장하도록
     return JsonResponse(data)
-
-    # try:
-    #     user = CustomUser.objects.get(email=email)
-    #     # 유저가 존재하는 경우
-    #     logger.warning(f"user: {user}")
-    #     logger.warning("유저 존재")
-    #     accept = requests.post("https://api.resumai.kr/accounts/kakao/login/finish/", data=data)
-    #     logger.warning(f"accept: {accept}")
-    #     logger.warning(f"accept.reason: {accept.reason}")
-    #     logger.warning(f"accept.history: {accept.history}")
-    #     logger.warning(accept.content)
-    #     accept_status = accept.status_code
-    #     logger.warning(accept_status)
-    #
-    #     if accept_status != 200:
-    #         return Response({"err_msg": "failed to signin"}, status=accept_status)
-    #
-    #     accept_json = accept.json()
-    #     logger.warning(f"accept_json, {accept_json}")
-    #     # key 이름 변경
-    #     accept_json["accessToken"] = accept_json.pop("access")
-    #     accept_json["refreshToken"] = accept_json.pop("refresh")
-    #     accept_json["userProfile"] = accept_json.pop("user")
-    #     accept_json["userProfile"]["id"] = accept_json["userProfile"].pop("pk")
-    #     return JsonResponse(accept_json)
-    #
-    # except CustomUser.DoesNotExist:
-    #     # 기존에 가입된 유저가 없으면 새로 가입
-    #     logger.warning("유저 미존재")
-    #     accept = requests.post("http://localhost:8000/accounts/kakao/login/finish/", data=data)
-    #     logger.warning(f"accept: {accept}")
-    #     logger.warning(f"accept.reason: {accept.reason}")
-    #     logger.warning(f"accept.request: {accept.request}")
-    #     logger.warning(f"accept.raw: {accept.raw}")
-    #     accept_status = accept.status_code
-    #     logger.warning(accept_status)
-    #     if accept_status != 200:
-    #         return Response({"err_msg": "failed to signup"}, status=accept_status)
-    #
-    #     # user의 pk, email, first name, last name과 Access Token, Refresh token 가져옴
-    #     accept_json = accept.json()
-    #     logger.warning(f"accept_json, {accept_json}")
-    #     # key 이름 변경
-    #     accept_json["accessToken"] = accept_json.pop("access")
-    #     accept_json["refreshToken"] = accept_json.pop("refresh")
-    #     accept_json["userProfile"] = accept_json.pop("user")
-    #     accept_json["userProfile"]["id"] = accept_json["userProfile"].pop("pk")
-    #     return JsonResponse(accept_json)
 
 
 @extend_schema(
