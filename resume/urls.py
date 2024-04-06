08e268f3b5cd4a896755b09b42eb8c77378f2728
yjoonjang <yjoonjang34@gmail.com>
2024-04-07 02:59:59 +0900

    fix: add logger

--- resume/urls.py
+++ resume/urls.py
@@ -3,6 +3,7 @@ from resume import views
 
 
 urlpatterns = [
+    path("all/", views.GetAllResumeView.as_view(), name="get_all_resume"),
     path("guidelines/", views.GetGuidelinesView.as_view(), name="get_guidelines"),
     path("generate/", views.GenerateResumeView.as_view(), name="generate_resume"),
     path("", views.PostResumeView.as_view(), name="post_resume"),
