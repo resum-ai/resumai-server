bc305a22822c3a469c13ce1735b717aa76455a75
yjoonjang <yjoonjang34@gmail.com>
2024-04-09 19:03:43 +0900

    fix: 모든 api url에 / 삭제

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,12 +3,12 @@ from accounts import views
 
 # TODO swagger에 뜨는 api 관리
 urlpatterns = [
-    path("kakao/", views.kakao_login, name="kakao_login"),
+    path("kakao", views.kakao_login, name="kakao_login"),
     path(
-        "kakao/login/",
+        "kakao/login",
         views.KakaoLoginView.as_view(),
         name="kakao_login_todjango",
     ),
-    path("update/", views.UpdateUserInfoView.as_view(), name="update_user_info"),
+    path("update", views.UpdateUserInfoView.as_view(), name="update_user_info"),
     path("user/me", views.GetUserInfoView.as_view(), name="get_user_info"),
 ]
--- resume/urls.py
+++ resume/urls.py
@@ -3,9 +3,9 @@ from resume import views
 
 
 urlpatterns = [
-    path("all/", views.GetAllResumeView.as_view(), name="get_all_resume"),
-    path("guidelines/", views.GetGuidelinesView.as_view(), name="get_guidelines"),
-    path("generate/", views.GenerateResumeView.as_view(), name="generate_resume"),
+    path("all", views.GetAllResumeView.as_view(), name="get_all_resume"),
+    path("guidelines", views.GetGuidelinesView.as_view(), name="get_guidelines"),
+    path("generate", views.GenerateResumeView.as_view(), name="generate_resume"),
     path("", views.PostResumeView.as_view(), name="post_resume"),
     path("update/<int:id>", views.UpdateResumeView.as_view(), name="update_resume"),
     path("scrap/<int:id>", views.ScrapResumeView.as_view(), name="scrap_resume"),
