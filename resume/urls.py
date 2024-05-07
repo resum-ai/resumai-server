76f6cd0d2278047faaa4283d71f48503d38d2b58
yjoonjang <yjoonjang34@gmail.com>
2024-05-07 15:32:45 +0900

    fix: api url에서 / 제거

--- resume/urls.py
+++ resume/urls.py
@@ -3,18 +3,18 @@ from resume import views
 
 
 urlpatterns = [
-    path("/all", views.GetAllResumeView.as_view(), name="get_all_resume"),
-    path("/guidelines", views.GetGuidelinesView.as_view(), name="get_guidelines"),
-    path("/generate", views.GenerateResumeView.as_view(), name="generate_resume"),
+    path("all", views.GetAllResumeView.as_view(), name="get_all_resume"),
+    path("guidelines", views.GetGuidelinesView.as_view(), name="get_guidelines"),
+    path("generate", views.GenerateResumeView.as_view(), name="generate_resume"),
     # path("", views.PostResumeView.as_view(), name="post_resume"),
-    path("/update/<int:id>", views.UpdateResumeView.as_view(), name="update_resume"),
-    path("/scrap/<int:id>", views.ScrapResumeView.as_view(), name="scrap_resume"),
-    path("/<int:id>/chat", views.ChatView.as_view(), name="chat"),
-    path("/<int:pk>", views.GetResumeView.as_view(), name="update_resume"),
+    path("update/<int:id>", views.UpdateResumeView.as_view(), name="update_resume"),
+    path("scrap/<int:id>", views.ScrapResumeView.as_view(), name="scrap_resume"),
+    path("<int:id>/chat", views.ChatView.as_view(), name="chat"),
+    path("<int:pk>", views.GetResumeView.as_view(), name="update_resume"),
     path(
-        "/<int:pk>/chatHistory",
+        "<int:pk>/chatHistory",
         views.GetChatHistoryView.as_view(),
         name="get_chat_history",
     ),
-    path("/delete/<int:pk>", views.DeleteResumeView.as_view(), name="delete_resume"),
+    path("delete/<int:pk>", views.DeleteResumeView.as_view(), name="delete_resume"),
 ]
